b6852f00e3e29fd37017ac0a292223c3900b69cc
Loskir <kl@sinker.ru>
2022-05-31 20:26:43 +0300

    Add test

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const {Bot, webhookCallback} = require('grammy')
 const bot = new Bot(process.env.TOKEN)
 bot.command('start', (ctx) => ctx.reply('Welcome! Up and running (cyclic)'))
 
+bot.command('test', async (ctx) => {
+  const s = Date.now()
+  await ctx.reply('hi!')
+  const t = Date.now() - s
+  return ctx.reply(`that took ${t}ms`)
+})
+
 const app = express()
 app.use(express.json())
 app.post('/'+process.env.TOKEN, webhookCallback(bot, 'express'))

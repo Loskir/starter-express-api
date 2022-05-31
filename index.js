8a7cf1c21cd1d0ad7c9661539c05b92419056ae2
Loskir <kl@sinker.ru>
2022-05-31 20:24:14 +0300

    Add grammY

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const {Bot, webhookCallback} = require('grammy')
+
+const bot = new Bot(process.env.TOKEN)
+bot.command('start', (ctx) => ctx.reply('Welcome! Up and running (cyclic)'))
+
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
+app.use(express.json())
+app.post('/'+process.env.TOKEN, webhookCallback(bot, 'express'))
 app.listen(process.env.PORT || 3000)
